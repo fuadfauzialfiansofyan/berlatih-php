45f127d484fa1c305deb61e51d2af6858eb2ddd0
fuadfauzialfiansofyan <77064577+fuadfauzialfiansofyan@users.noreply.github.com>
2021-07-19 13:09:08 +0800

    commit git pertama

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bio Data</title>
+</head>
+<body>
+    <h1 style="border-bottom: 1px solid;" >BioData</h1>
+    <h3>Nama : Fuad Fauzi Alfian sofyan</h3>
+    <h3>Alamat : Makassar</h3>
+    <h3 >Email : fuadfauzialfiansofyan@gmail.com</h3>
+
+</body>
+</html>
